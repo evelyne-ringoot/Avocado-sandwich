66887b04d90115093b5c1293c8a8ad3170e7c1bc
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-04-07 14:41:32 -0400

    benchmarking limiting sizes

--- SVD2/benchmark/benchmark.jl
+++ SVD2/benchmark/benchmark.jl
@@ -38,12 +38,12 @@ function cusvdwithcopy(a)
 end
 
 elty=Float32
-sizes=[64,128,256,512,1024,2048,4096,8192,16384]
+sizes=[64,128,256,512,1024,2048,4096]
 timings=ones(4,length(sizes))*1000000
 errors=zeros(2,length(sizes))
 
 
-inputs=[randn(Float32,size_i, size_i) for size_i in sizes]
+inputs=[randn(elty,size_i, size_i) for size_i in sizes]
 println( "Checking correctness OOC");
 for (i,size_i) in enumerate(sizes[1:6])
     aout=OOC_SVD!(copy(inputs[i]), backend,kswitch=10, tilesinmem=14)
@@ -74,7 +74,7 @@ end
 
 
 
-inputs=[CUDA.randn(Float32,size_i, size_i) for size_i in sizes]
+inputs=[CUDA.randn(elty,size_i, size_i) for size_i in sizes]
 println( "Checking correctness GPU only");
 for (i,size_i) in enumerate(sizes[1:6])
     aout=mygesvd!(copy(inputs[i]))
@@ -83,22 +83,22 @@ for (i,size_i) in enumerate(sizes[1:6])
     errors[1,i]= norm((aout-aref)./aref)/sqrt(size_i)
 end
 println( "Benchmarking CUDA only");
-for (i,size_i) in enumerate(sizessizes[1:6])
+for (i,size_i) in enumerate(sizes)
     a=inputs[i]
     timings[1,i] = min( benchmark_ms(svdvals!, a,  alg=CUDA.CUSOLVER.QRAlgorithm()), timings[1,i])
 end
 
-for (i,size_i) in enumerate(sizes[1:8])
+for (i,size_i) in enumerate(sizes)
     a=inputs[i]
     timings[1,i] = min( benchmark_ms(svdvals!, a,  alg=CUDA.CUSOLVER.QRAlgorithm()), timings[1,i])
 end
 println( "Benchmarking KA only");
-for (i,size_i) in enumerate(sizes[1:8])
+for (i,size_i) in enumerate(sizes)
     a=inputs[i]
     timings[2,i] = min( benchmark_ms(mygesvd!,a), timings[2,i])
 end
 
-for (i,size_i) in enumerate(sizes[1:8])
+for (i,size_i) in enumerate(sizes)
     a=inputs[i]
     timings[2,i] = min( benchmark_ms(mygesvd!,a), timings[2,i])
 end
@@ -118,27 +118,3 @@ for (i,size_i) in enumerate(sizes)
     @printf " %4d   %8.02e    %8.02f  %8.02f  \n" size_i errors[2,i] timings[4,i] timings[3,i]
 end  
 
-#=
-timings=[    1.38     3.92      0.41
-3.96     8.0       4.57
-13.5     16.84     35.6
-47.53    49.86    287.58
-165.58   108.01   3185.4
-689.62   284.77  26037.0
-3841.68   891.77      0.0
-27813.8   6717.43      0.0
-205247       0         0]
-
-xaxis=sizes
-xaxist=[string.(xaxis[1:5]);"2k";"4k"; "8k";"16k";"32k"]
-yaxis=[1, 10, 100, 1e3,1e4, 6e4]
-yaxist=["1 ms","10ms","0.1s","1s", "10s", "1min"]
-
- plot(xaxis[1:8], timings[1:8,2], labels= "", lw=2, markershape=:circle, markerstrokewidth=0, markersize=3)
- plot!(xaxis[1:6], timings[1:6,3], labels="",lw=2, markershape=:circle, markerstrokewidth=0, markersize=3 )
- plot!(xaxis[1:9],timings[:,1], label= "" ,lw=2, markershape=:circle, markerstrokewidth=0, markersize=3)
- plot!(xaxis=:log2,  yaxis=:log10, xticks=(xaxis, xaxist), yticks=(yaxis,yaxist),xlabel= "matrix size nxn", ylabel= "Execution time", dpi=1000)
- plot!( size = (600, 300))
- plot!([32000,32000],[1,1], label="")
- savefig("benchmark.png")
- =#
