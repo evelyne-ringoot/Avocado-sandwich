29308448d2cb9e837f61e2ed7abca9e4f94c6648
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-04-16 14:28:37 -0400

    simplifying tests

--- KAbasedSVD/test/test_tiledalgo.jl
+++ KAbasedSVD/test/test_tiledalgo.jl
@@ -1,3 +1,4 @@
+using Test, Random
 include(joinpath("..","src","QRkernels.jl"))
 TILE_SIZE1=1
 TILE_SIZE2=1 
@@ -7,61 +8,62 @@ include(joinpath("..","src","TiledMatrix.jl"))
 include(joinpath("..","src","bulgechasing.jl"))
 include(joinpath("..","src","tiledalgos.jl"))
 
-@testset "Tiled algos elty = $elty and 2D=$dims2d and n=$tilesize" for 
-                                                elty in [ Float32, Float64],
-                                                tilesize in [4,16],
+@testset "Tiled algos 2D=$dims2d and n=$tilesize" for  tilesize in [4,16], 
                                                  (mydims2d,tilesize,tilefactor ) in ( (false, tilesize,1), (true, tilesize, 4) )
-
-    compile_kernels(tilesize=tilesize, tilefactor=tilefactor, tiledim2d=mydims2d)
-    no_blocks=6
-    backend=KernelAbstractions.get_backend(CUDA.randn(2))
-    A=rand!(allocate(backend, elty,tilesize*no_blocks, tilesize*no_blocks))
     
-    @testset "QR" begin
-        testmatrix=copy(A)
-        refmatrix=copy(A)
-        Atile = TiledMatrix(testmatrix,tilesize,tilesize, tiledim2d= mydims2d ? TILE_SIZE2[2] : 0)
-        tiled_QR!(Atile)
-        @test triu(testmatrix) .*sign.(diag(testmatrix)) ≈  qr(refmatrix).R .* sign.(diag( qr(refmatrix).R))
-    end
+    compile_kernels(tilesize=tilesize, tilefactor=tilefactor, tiledim2d=mydims2d)
+    backend=KernelAbstractions.get_backend(CUDA.randn(2))
 
-    @testset "SVD" begin
-        testmatrix=copy(A)
-        refmatrix=copy(A)
-        Atile = TiledMatrix(testmatrix,tilesize,tilesize, tiledim2d= dims2d ? TILE_SIZE2[2] : 0)
-        Blockbidiag!(Atile)
-        @test svdvals(Atile.A) ≈ svdvals(refmatrix)
+    @testset "elty = $elty and 2D=$dims2d and n=$tilesize and noblocks=$no_blocks" for elty in [ Float32, Float64], no_blocks in [4,6]
+        
+        @testset "GPU- only QR and SVD" begin
+            A=rand!(allocate(backend, elty,tilesize*no_blocks, tilesize*no_blocks))
+            
+            testmatrix=copy(A)
+            refmatrix=copy(A)
+            Atile = TiledMatrix(testmatrix,tilesize,tilesize, tiledim2d= mydims2d ? TILE_SIZE2[2] : 0)
+            tiled_QR!(Atile)
+            @test triu(testmatrix) .*sign.(diag(testmatrix)) ≈  qr(refmatrix).R .* sign.(diag( qr(refmatrix).R))
+            
+            testmatrix=copy(A)
+            refmatrix=copy(A)
+            Atile = TiledMatrix(testmatrix,tilesize,tilesize, tiledim2d= dims2d ? TILE_SIZE2[2] : 0)
+            Blockbidiag!(Atile)
+            @test svdvals(Atile.A) ≈ svdvals(refmatrix)
+        end
+
+        @testset "OOC-only QR and SVD" begin
+            A=randn(elty,tilesize*no_blocks, tilesize*no_blocks)
+
+            testmatrix=copy(A)
+            refmatrix=copy(A)
+            Atile = LargeTiledMatrix(testmatrix, backend, tilesize*no_blocks, tilesize, no_blocks, tiledim2d= mydims2d ? TILE_SIZE2[2] : 0) 
+            tiled_QR!(Atile)
+            @test triu(testmatrix) .*sign.(diag(testmatrix)) ≈  qr(refmatrix).R .* sign.(diag( qr(refmatrix).R))
+
+            testmatrix=copy(A)
+            refmatrix=copy(A)
+            Atile = LargeTiledMatrix(testmatrix, backend, tilesize*no_blocks, tilesize, no_blocks, tiledim2d= mydims2d ? TILE_SIZE2[2] : 0) 
+            Blockbidiag!(Atile)
+            @test svdvals(Atile.A) ≈ svdvals(refmatrix)
+
+        end
+
+        @testset "OOC+GPU QR and SVD" begin
+            A=randn(elty,tilesize*no_blocks, tilesize*no_blocks)
+            
+            testmatrix=copy(A)
+            refmatrix=copy(A)
+            OOC_QR!(testmatrix, kswitch=3, mydims2d=mydims2d,tilesize=tilesize,tilefactor=tilefactor, backend=backend)
+            @test triu(testmatrix) .*sign.(diag(testmatrix)) ≈  qr(refmatrix).R .* sign.(diag( qr(refmatrix).R))
+
+            testmatrix=copy(A)
+            refmatrix=copy(A)
+            Atile = LargeTiledMatrix(testmatrix, backend, tilesize*no_blocks, tilesize, no_blocks, tiledim2d= mydims2d ? TILE_SIZE2[2] : 0) 
+            OOC_Bidiag!(testmatrix, kswitch=3, mydims2d=mydims2d,tilesize=tilesize,tilefactor=tilefactor, backend=backend)
+            @test svdvals(Atile.A) ≈ svdvals(refmatrix)
+
+        end
     end
 
 end
-
-    @testset "Large Tiled algos elty = $elty and 2D=$dims2d and n=$tilesize" for 
-                        elty in [ Float32, Float64],
-                        tilesize in [4,16],
-                        (mydims2d,tilesize,tilefactor ) in ( (false, tilesize,1), ( true, tilesize, 4) )
-
-        compile_kernels(tilesize=tilesize, tilefactor=tilefactor, tiledim2d=tiledim2d)
-        no_blocks=3
-        backend=KernelAbstractions.get_backend(CUDA.randn(2))
-        A=randn(elty,tilesize*no_blocks, tilesize*no_blocks)
-
-        @testset "QR" begin
-        testmatrix=copy(A)
-        refmatrix=copy(A)
-        Atile = LargeTiledMatrix(testmatrix, backend, tilesize*no_blocks, tilesize, no_blocks, tiledim2d= dims2d ? TILE_SIZE2[2] : 0) 
-        tiled_QR!(Atile)
-        @test triu(testmatrix) .*sign.(diag(testmatrix)) ≈  qr(refmatrix).R .* sign.(diag( qr(refmatrix).R))
-        end
-
-        @testset "SVD" begin
-        testmatrix=copy(A)
-        refmatrix=copy(A)
-        Atile = LargeTiledMatrix(testmatrix, backend, tilesize*no_blocks, tilesize, no_blocks, tiledim2d= dims2d ? TILE_SIZE2[2] : 0) 
-        Blockbidiag!(Atile)
-        @test svdvals(Atile.A) ≈ svdvals(refmatrix)
-        end
-
-    end
-
-
-       
