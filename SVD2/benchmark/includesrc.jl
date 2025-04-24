709eeb89ff657b49da75895508bcb70e3586631d
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-04-23 22:07:08 -0400

    adding loading previous funcs

---
+++ SVD2/benchmark/includesrc.jl
@@ -0,0 +1,8 @@
+
+BLAS.set_num_threads(Threads.nthreads())
+include("../src/cusol_funcs.jl")
+include("../src/KAfuncs.jl")
+include("../src/qr_kernels.jl")
+include("../src/brdgpu.jl")
+include("../src/datacomms.jl")
+include("../src/tiledalgos.jl")
