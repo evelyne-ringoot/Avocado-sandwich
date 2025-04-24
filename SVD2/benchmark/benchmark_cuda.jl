c002ca6fef296f60b2704f4523569b1c247b7835
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-04-23 20:33:24 -0400

    adding more benchmarking sizes

--- SVD2/benchmark/benchmark_cuda.jl
+++ SVD2/benchmark/benchmark_cuda.jl
@@ -12,11 +12,11 @@ elty=Float32
 include("includesrc.jl")
 include("benchfuncs.jl")
 
-sizes=[64,128,256,512,1024,2048, 4096]#,8192,8192*2]
+sizes=[64,128,256,512,1024,2048, 4096,8192,8192*2]
 include("benchmarkgeneral.jl")
 
-sizes=[64,128,256,512,1024,2048, 4096]#,8192,8192,8192*2,8192*4, 8192*8]
+sizes=[64,128,256,512,1024,2048, 4096,8192,8192,8192*2,8192*4, 8192*8]
 include("benchmarkmataddmul.jl")
 
-sizes=8192 .*[1]#,2,4,8]
+sizes=8192 .*[1,2,4,8]
 include("benchmarkgenerallarge.jl")
