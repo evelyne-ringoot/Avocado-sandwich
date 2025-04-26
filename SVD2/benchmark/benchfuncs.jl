f1486e61963cdfd84ce74a94e8ad7a550a99b7c0
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-04-26 02:26:48 -0400

    make runtime variable and increase matadd and mul sizes

--- SVD2/benchmark/benchfuncs.jl
+++ SVD2/benchmark/benchfuncs.jl
@@ -1,10 +1,13 @@
+const MINTIME = 200.0
+const NUMRUMS= 12
+
 function benchmark_ms( size_i, myfunc)
     a=randn!(KernelAbstractions.zeros(backend, elty,size_i, size_i))
     elapsed=0.0
     best=10000000000
     i=0
-    numruns = (size_i < 1025) ? 12 : 2
-    while(elapsed<200.0 || (i<2 &&elapsed<5000.0))
+    numruns = (size_i < 1025) ? NUMRUMS : 2
+    while(elapsed<MINTIME || (i<2 &&elapsed<5000.0))
         KernelAbstractions.synchronize(backend)
         start = time_ns()
         for i=1:numruns
@@ -49,8 +52,8 @@ function benchmark_ms_muladd( size_i, myfunc)
     elapsed=0.0
     best=100000000
     i=0
-    numruns = (size_i < 4097) ? 12 : 2
-    while(elapsed<200.0 || (i<2 &&elapsed<5000.0))
+    numruns = (size_i < 1024*16+1) ? NUMRUMS : 2
+    while(elapsed<MINTIME || (i<2 &&elapsed<5000.0))
         KernelAbstractions.synchronize(backend)
         start = time_ns()
         for i=1:numruns
