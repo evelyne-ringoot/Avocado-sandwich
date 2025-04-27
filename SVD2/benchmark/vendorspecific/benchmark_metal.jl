e09c0bc0cb5b26609805c43a9d3f8b1b1945cebe
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-04-27 00:21:20 -0400

    conversion to single precision for vendor functions

--- SVD2/benchmark/vendorspecific/benchmark_metal.jl
+++ SVD2/benchmark/vendorspecific/benchmark_metal.jl
@@ -2,4 +2,7 @@
 using Metal
 Metal.versioninfo()
 const backend=KernelAbstractions.get_backend( MtlArray([1]))
-@inline vendorsvd!(input::MtlArray) = svdvals!(Array(input))
+@inline vendorsvd!(input::MtlArray) = svdvals!((Array(input)))
+if (ARGS[2]!="S")
+    @inline vendorsvd!(input::MtlArray) = svdvals!(Float32.(Array(input)))
+end
--- SVD2/benchmark/vendorspecific/benchmark_oneapi.jl
+++ SVD2/benchmark/vendorspecific/benchmark_oneapi.jl
@@ -3,4 +3,7 @@ using oneAPI
 oneAPI.versioninfo()
 const backend=KernelAbstractions.get_backend(oneArray(rand(Float32, 2,2)))
 @inline vendorsvd!(input::oneArray) = oneAPI.gesvd!('N','N',input)
+if (ARGS[2]=="H")
+    @inline vendorsvd!(input::oneArray) = oneAPI.gesvd!('N','N',Float32.(input))
+end
 
