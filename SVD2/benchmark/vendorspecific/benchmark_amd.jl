d33f4c6636bc54ad34dedd0069b3a954531d5675
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-06-02 06:27:01 -0400

    commenting printing of vendorinfo

--- SVD2/benchmark/vendorspecific/benchmark_amd.jl
+++ SVD2/benchmark/vendorspecific/benchmark_amd.jl
@@ -1,6 +1,6 @@
 
 using AMDGPU
-AMDGPU.versioninfo()
+#AMDGPU.versioninfo()
 const backend=KernelAbstractions.get_backend(AMDGPU.zeros(2))
 @inline vendorsvd!(input::ROCArray) = AMDGPU.rocSOLVER.gesvd!('N','N',input)[2]
 if (ARGS[2]=="H")
--- SVD2/benchmark/vendorspecific/benchmark_oneapi.jl
+++ SVD2/benchmark/vendorspecific/benchmark_oneapi.jl
@@ -1,6 +1,6 @@
 
 using oneAPI
-oneAPI.versioninfo()
+#oneAPI.versioninfo()
 const backend=KernelAbstractions.get_backend(oneArray(rand(Float32, 2,2)))
 @inline vendorsvd!(input::oneArray) = oneAPI.gesvd!('N','N',input)
 if (ARGS[2]=="H")
