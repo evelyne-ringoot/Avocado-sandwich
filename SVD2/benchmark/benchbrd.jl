cec057000ca644f55e34c0ff7db736de025d8c68
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-06-03 01:18:36 -0400

    fixing issues and adding packed format

--- SVD2/benchmark/benchbrd.jl
+++ SVD2/benchmark/benchbrd.jl
@@ -6,42 +6,27 @@ include("benchfuncs.jl")
 
 print("starting code : ")
 println(Dates.format(now(), "HH:MM:SS")  )
-sizes=[128,256,512,1024,2048,4096,8192 ]#,,  32768, 65536] # 2048, 4096,8192
-timings=ones(length(sizes))*1000000000
-errors=zeros(length(sizes))
 size_i=4BW
 b=tril(triu(randn!(KernelAbstractions.zeros(backend,elty,size_i,size_i))),BW)
-print("matrix generated, compiling started at : ")
+print("matrix generated, compiling at : ")
 println(Dates.format(now(), "HH:MM:SS")  )
-mygbbrd4!(b)
+mygbbrd!(b)
+mygbbrd_packed!(b)
+mygbbrd_packed_nocomm!(b)
 print("first compile done at : ")
 println(Dates.format(now(), "HH:MM:SS")  )
 
-#=
-n=size_i
-    Int(BW/TILESIZE):-1:1
-        A=copy(b)
-        mygbbrd4!(A)
-        KernelAbstractions.synchronize(backend)
-        norm(svdvals(A)-svdvals(b))/norm(svdvals(b))
-        1:(n-1)
-        k=1
-        bwiter=2
 
-    k=1
-        brd4!(view(A,k:n,k:n),min(k,1+cld((n-k), (3TILESIZE-1))),bwiter)
-                for k in 1:(n-1)
-            brd4!(view(A,k:n,k:n),min(k,1+cld((n-k), (3TILESIZE*bwiter-1))),bwiter)
-        end
-=#
-
+sizes=[128,256,512,1024,2048,4096,8192 ]
+timings=ones(length(sizes))*1000000000
+errors=zeros(length(sizes))
 
 print( "Checking correctness at : ")
 println(Dates.format(now(), "HH:MM:SS")  )
 for (i,size_i) in enumerate(sizes)
     a=tril(triu(randn!(KernelAbstractions.zeros(backend,elty,size_i,size_i))),BW)
     aref=svdvals(Float64.(a), alg=CUDA.CUSOLVER.QRAlgorithm())
-    mygbbrd4!(a)
+    mygbbrd!(a)
     KernelAbstractions.synchronize(backend)
     aout=svdvals(Float64.(a), alg=CUDA.CUSOLVER.QRAlgorithm())
     errors[i]=norm(aref-aout)/norm(aout)
@@ -49,16 +34,10 @@ end
 print("done accuracy : ")
 println(Dates.format(now(), "HH:MM:SS")  )
 
-#=
-println( "warump KA only");
-for (i,size_i) in enumerate(sizes)
-    timings[i] = min( benchmark_ms(size_i,mygbbrd3!), timings[i])
-end
-=#
 print( "becnhmark KA at : ")
 println(Dates.format(now(), "HH:MM:SS")  )
 for (i,size_i) in enumerate(sizes)
-    timings[i] = min( benchmark_ms_large(size_i,mygbbrd4!), timings[i])
+    timings[i] = min( benchmark_ms(size_i,mygbbrd!), timings[i])
 end
 print("done at : ")
 println(Dates.format(now(), "HH:MM:SS")  )
@@ -68,4 +47,76 @@ println( " size    RRMSE    time (ms)  ");
 println(" ------  --------  ----------   ");
 for (i,size_i) in enumerate(sizes)
     @printf " %4d   %8.02e    %8.02f \n" size_i errors[i] timings[i] 
+end  
+
+
+timings=ones(length(sizes))*1000000000
+errors=zeros(length(sizes))
+
+print( "Checking correctness packed at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+for (i,size_i) in enumerate(sizes)
+    a=tril(triu(randn!(KernelAbstractions.zeros(backend,elty,size_i,size_i))),BW)
+    aref=svdvals(Float64.(a), alg=CUDA.CUSOLVER.QRAlgorithm())
+    mygbbrd_packed!(a)
+    KernelAbstractions.synchronize(backend)
+    aout=svdvals(Float64.(a), alg=CUDA.CUSOLVER.QRAlgorithm())
+    errors[i]=norm(aref-aout)/norm(aout)
+end
+print("done accuracy packed : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+
+print( "becnhmark KA packed (incl communication)  at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+for (i,size_i) in enumerate(sizes)
+    timings[i] = min( benchmark_ms(size_i,mygbbrd_packed!), timings[i])
+end
+print("done packed at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+
+println("BRD packed incl communication");
+println( " size    RRMSE    time (ms)  ");
+println(" ------  --------  ----------   ");
+for (i,size_i) in enumerate(sizes)
+    @printf " %4d   %8.02e    %8.02f \n" size_i errors[i] timings[i] 
+end  
+
+
+timings=ones(length(sizes))*1000000000
+
+print( "becnhmark KA packed (excl communication) at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+for (i,size_i) in enumerate(sizes)
+    timings[i] = min( benchmark_ms(size_i,mygbbrd_packed_nocomm!, 3BW+1), timings[i])
+end
+print("done packed at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+
+println("BRD packed excl communication");
+println( " size     time (ms)  ");
+println(" ------  ----------   ");
+for (i,size_i) in enumerate(sizes)
+    @printf " %4d   %8.02f \n" size_i  timings[i] 
+end  
+
+sizes=[1,2,4,8,16].*8192 
+timings=ones(length(sizes))*1000000000
+
+print( "becnhmarking large sizes (excl communication) at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+try 
+    for (i,size_i) in enumerate(sizes)
+        timings[i] = min( benchmark_ms_large(size_i,mygbbrd_packed_nocomm!, 3BW+1), timings[i])
+    end
+    print("done packed at : ")
+    println(Dates.format(now(), "HH:MM:SS")  )
+catch e 
+    println("!!! did not finish all sizes")
+end
+
+println("BRD packed large excl communication");
+println( " size   time (ms)  ");
+println(" ------   ----------   ");
+for (i,size_i) in enumerate(sizes)
+    @printf " %4d   %8.02f \n" size_i  timings[i] 
 end  
