44bf3ea8a459be1e66e36168b605736a2e9710b6
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-06-05 03:28:49 -0400

    compatibility

--- SVD2/benchmark/benchfuncs.jl
+++ SVD2/benchmark/benchfuncs.jl
@@ -1,8 +1,8 @@
 
 
 function benchmark_ms( size_i, myfunc, dim2::Int=size_i)
-    a=randn!(KernelAbstractions.zeros(backend, elty,dim2, size_i))
-    b=randn!(KernelAbstractions.zeros(backend, elty,dim2, size_i))
+    a=arty(randn!(zeros(backend, elty,dim2, size_i)))
+    b=arty(randn!(zeros(backend, elty,dim2, size_i)))
     elapsed=0.0
     best=10000000000
     i=0
@@ -28,8 +28,8 @@ end
 
 
 function benchmark_ms_large( size_i, myfunc, dim2::Int=size_i)
-    a=randn!(KernelAbstractions.zeros(backend, elty,dim2, size_i))
-    b=randn!(KernelAbstractions.zeros(backend, elty,dim2, size_i))
+    a=arty(randn!(zeros(backend, elty,dim2, size_i)))
+    b=arty(randn!(zeros(backend, elty,dim2, size_i)))
     elapsed=0.0
     best=1000000000
     i=0
@@ -50,9 +50,9 @@ function benchmark_ms_large( size_i, myfunc, dim2::Int=size_i)
 end
 
 function benchmark_ms_muladd( size_i, myfunc)
-    a=rand!(KernelAbstractions.zeros(backend, elty,size_i, size_i))
-    b=rand!(KernelAbstractions.zeros(backend, elty,size_i, size_i))
-    c=rand!(KernelAbstractions.zeros(backend, elty,size_i, size_i))
+    a=arty(rand!(zeros(backend, elty,size_i, size_i)))
+    b=arty(rand!(zeros(backend, elty,size_i, size_i)))
+    c=arty(rand!(zeros(backend, elty,size_i, size_i)))
     elapsed=0.0
     best=100000000
     i=0
