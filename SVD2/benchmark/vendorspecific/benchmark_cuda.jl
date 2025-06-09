5b1219939be70e0afb7c6ddd6db479035b7f8f4d
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-06-09 14:06:45 -0400

    fixing weird fp16 cuda copy sync issue

--- SVD2/benchmark/vendorspecific/benchmark_cuda.jl
+++ SVD2/benchmark/vendorspecific/benchmark_cuda.jl
@@ -7,5 +7,5 @@ KernelAbstractions.get_backend(CUDA.zeros(1))
 const backend=CUDABackend(false, false, true)
 @inline vendorsvd!(input::CuArray) = svdvals!(input,  alg=CUDA.CUSOLVER.QRAlgorithm())
 if (elty==Float16)
-    @inline vendorsvd!(input::CuArray) = svdvals!(Float32.(input),  alg=CUDA.CUSOLVER.QRAlgorithm())
+    @inline vendorsvd!(input::CuArray) = svdvals!(copy(Float32.(input)),  alg=CUDA.CUSOLVER.QRAlgorithm())
 end
