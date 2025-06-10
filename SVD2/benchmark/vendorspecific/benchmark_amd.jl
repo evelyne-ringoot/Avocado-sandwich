70679f23ab4ec42cc184e6b3a41bc6e291fc4f50
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-06-10 13:34:23 -0400

    cross platform fp16 stability

--- SVD2/benchmark/vendorspecific/benchmark_amd.jl
+++ SVD2/benchmark/vendorspecific/benchmark_amd.jl
@@ -4,5 +4,5 @@ using AMDGPU
 const backend=KernelAbstractions.get_backend(AMDGPU.zeros(2))
 @inline vendorsvd!(input::ROCArray) = AMDGPU.rocSOLVER.gesvd!('N','N',input)[2]
 if (ARGS[2]=="H")
-    @inline vendorsvd!(input::ROCArray) = AMDGPU.rocSOLVER.gesvd!('N','N',Float32.(input))[2]
+    @inline vendorsvd!(input::ROCArray) = AMDGPU.rocSOLVER.gesvd!('N','N',copy(Float32.(input)))[2]
 end
--- SVD2/benchmark/vendorspecific/benchmark_oneapi.jl
+++ SVD2/benchmark/vendorspecific/benchmark_oneapi.jl
@@ -2,8 +2,7 @@
 using oneAPI
 #oneAPI.versioninfo()
 const backend=KernelAbstractions.get_backend(oneArray(rand(Float32, 2,2)))
-@inline vendorsvd!(input::oneArray) = oneAPI.gesvd!('N','N',input)
+@inline vendorsvd!(input::oneArray) = oneMKL.gesvd!('N','N',input)[2]
 if (ARGS[2]=="H")
-    @inline vendorsvd!(input::oneArray) = oneAPI.gesvd!('N','N',Float32.(input))
+    @inline vendorsvd!(input::oneArray) = oneMKL.gesvd!('N','N',copy(Float32.(input)))
 end
-
