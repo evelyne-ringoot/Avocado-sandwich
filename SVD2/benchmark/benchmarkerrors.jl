bdd6f81311a35838fd8924aff45cb2dff2ced165
Evelyne <eringoot@mit.edu>
2025-06-24 18:20:19 +0200

    catch unfinished tests

--- SVD2/benchmark/benchmarkerrors.jl
+++ SVD2/benchmark/benchmarkerrors.jl
@@ -1,34 +1,39 @@
 
 errors=zeros(4,length(sizes))
-println( "Checking correctness GPU only")
-for (i,size_i) in enumerate(sizes)
-    input=arty(randn!(zeros(elty,size_i, size_i)))
-    aband=copy(input)
-    myblockdiag!(aband)
-    KernelAbstractions.synchronize(get_backend(input))
-    abandfp64=Float64.(copy(aband))
-    KernelAbstractions.synchronize(get_backend(input))
-    abandsvd= vendorsvd!(abandfp64)
-    mygbbrd!(aband)
-    KernelAbstractions.synchronize(get_backend(input))
-    abidiagsvd= vendorsvd!(Float64.(copy(aband)))
-    aout=mygesvd!(copy(input))
-    aref=vendorsvd!(copy(input))
-    areffp64=vendorsvd!(Float64.(copy(input)))
-    KernelAbstractions.synchronize(backend)
-    if (!isnothing(aref))
-        output=KernelAbstractions.zeros(backend,elty,size_i)
-        output.=aout #Array because mygesvd returns CPU Array
-        errors[1,i]= norm((output-areffp64))/norm(areffp64)
-        errors[2,i]= norm((abandsvd-areffp64))/norm(areffp64)
-        errors[3,i]= norm((abidiagsvd-areffp64))/norm(areffp64)
-        errors[4,i]= norm((aref-areffp64))/norm(areffp64)
+print( "Checking correctness GPU only at : ")
+println(Dates.format(now(), "HH:MM:SS")  )
+try
+    for (i,size_i) in enumerate(sizes)
+        input=arty(randn!(zeros(elty,size_i, size_i)))
+        aband=copy(input)
+        myblockdiag!(aband)
+        KernelAbstractions.synchronize(get_backend(input))
+        abandfp64=Float64.(copy(aband))
+        KernelAbstractions.synchronize(get_backend(input))
+        abandsvd= vendorsvd!(abandfp64)
+        mygbbrd!(aband)
+        KernelAbstractions.synchronize(get_backend(input))
+        abidiagsvd= vendorsvd!(Float64.(copy(aband)))
+        aout=mygesvd!(copy(input))
+        aref=vendorsvd!(copy(input))
+        areffp64=vendorsvd!(Float64.(copy(input)))
+        KernelAbstractions.synchronize(backend)
+        if (!isnothing(aref))
+            output=KernelAbstractions.zeros(backend,elty,size_i)
+            copyto!(output,aout) #Array because mygesvd returns CPU Array
+            errors[1,i]= norm((output-areffp64))/norm(areffp64)
+            errors[2,i]= norm((abandsvd-areffp64))/norm(areffp64)
+            errors[3,i]= norm((abidiagsvd-areffp64))/norm(areffp64)
+            errors[4,i]= norm((aref-areffp64))/norm(areffp64)
+        end
     end
+catch e
+    print("unfinished at : ")
+    println(Dates.format(now(), "HH:MM:SS")  )
+finally
+    println( " size    RRMSE svd  RRMSE band  RRMSE BRD  RRMSE CUSOLVER");
+    println(" ------  --------  ----------  ----------  ---------- ");
+    for (i,size_i) in enumerate(sizes)
+        @printf " %4d   %8.02e    %8.02e  %8.02e   %8.02e \n" size_i errors[1,i] errors[2,i] errors[3,i] errors[4,i] 
+    end  
 end
-
-println("GPU only SVD");
-println( " size    RRMSE svd  RRMSE band  RRMSE BRD  RRMSE CUSOLVER");
-println(" ------  --------  ----------  ----------  ---------- ");
-for (i,size_i) in enumerate(sizes)
-    @printf " %4d   %8.02e    %8.02e  %8.02e   %8.02e \n" size_i errors[1,i] errors[2,i] errors[3,i] errors[4,i] 
-end  
