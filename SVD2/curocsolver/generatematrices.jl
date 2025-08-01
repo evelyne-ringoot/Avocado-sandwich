be08d727e7f7c7b4f682da58a35accf0394e184e
Evelyne <110474206+evelyne-ringoot@users.noreply.github.com>
2025-07-31 21:49:09 -0500

    accuracy fix

--- SVD2/curocsolver/generatematrices.jl
+++ SVD2/curocsolver/generatematrices.jl
@@ -3,13 +3,13 @@ using LinearAlgebra
 sizes = [128,256,512,1024,2048,4096,8192]
 for n in sizes
     svdvals= [((1:-1/(n-1):0))...]
-    unit1= qr!(randn(Float32,n,n)).Q
-    unit2= qr!(randn(Float32,n,n)).Q
+    unit1= qr!(randn(Float64,n,n)).Q
+    unit2= qr!(randn(Float64,n,n)).Q
     a= unit1*diagm(svdvals)*unit2
     open("data_"*string(n)*".bin", "w") do f
-        write(f, a)
+        write(f, Float32.(a))
     end
     open("data_"*string(n)*"_svd.bin", "w") do f
-        write(f, svdvals)
+        write(f, Float32.(svdvals))
     end
 end
